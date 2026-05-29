debae72ac5c4cb48b1a99ff1fe9b8293ef780597
Konda Balaji Rao <balajikonda9046@gmail.com>
2026-05-29 11:22:39 +0530

    testing git commit --no-verify

--- testing.py
+++ testing.py
@@ -1,7 +1,12 @@
 testing git commands for staging and commiting
+git init
+git add
+git add .
+git add -A 
 git reset
 git commit --amend
 git commit --all
 git commit -a
 git reset --soft HEAD~1
 git reset --hard HEAD~1
+git commit --no-verify
